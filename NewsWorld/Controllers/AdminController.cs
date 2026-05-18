1aec4c4b719b03b84f33a7809ddb6a325b3f19bb
Mihir <Mihir@DESKTOP-DK4JAT2>
2026-05-18 19:15:42 +0530

    Add project files.

---
+++ NewsWorld/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsWorld.Data;       
+using NewsWorld.Models;     
+using System.Linq;
+
+namespace NewsWorld.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Constructor
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // ✅ POST: Admin/Login
+        [HttpPost]
+        public IActionResult Login(string Username, string Password)
+        {
+            var admin = _context.Admins
+                .FirstOrDefault(a => a.Username == Username && a.Password == Password);
+
+            if (admin != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Invalid Username or Password";
+            return View();
+        }
+    }
+}
---
+++ NewsWorld/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsWorld.Models;
+
+namespace NewsWorld.Data   // ✅ MUST match this
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
